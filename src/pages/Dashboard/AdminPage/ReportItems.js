f759322c4bf876264499a238a064eb4e18890781
Shariful Islam <engr.shariful.cse@gmail.com>
2022-12-21 22:38:32 +0100

    added report item page

--- src/pages/Dashboard/AdminPage/ReportItems.js
+++ src/pages/Dashboard/AdminPage/ReportItems.js
@@ -4,6 +4,9 @@ const ReportItems = () => {
   return (
     <div>
       <h2 className="text-3xl">Report Items</h2>
+      <div>
+        <h1 className="text-5xl mt-40 ml-40">Commig soon...</h1>
+      </div>
     </div>
   );
 };
